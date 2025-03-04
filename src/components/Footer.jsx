213ea73a6a1a6fce2c4eaa4eb0f497adf7c39acc
Zachary Betters <100448323+zbetters97@users.noreply.github.com>
2025-03-04 14:38:35 -0500

    0.0.2 Update
    -Updated footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,15 +7,15 @@ export default function Footer() {
   return (
     <footer>
       <p className="footer-info">
-        Coded in
+        Coded in&nbsp;
         <a
           className="anchor-text"
           href="https://code.visualstudio.com"
           target="_blank"
         >
-          Visual Studio Code
+          Visual Studio Code&nbsp;
         </a>
-        by
+        by&nbsp;
         <a
           href="https://www.linkedin.com/in/zachary-betters-916a74116/"
           target="_blank"
@@ -23,13 +23,15 @@ export default function Footer() {
         >
           Zachary Betters
         </a>
-        .<br />
-        Built with HTML & SCSS, and deployed with
+        .&nbsp;
+        <br />
+        Built with React.JS, HTML, & SCSS, and deployed with&nbsp;
         <a href="https://www.netlify.com" target="_blank" className="anchor-text">
           Netlify
         </a>
-        .<br />
-        All text is set in the
+        .
+        <br />
+        All text is set in the&nbsp;
         <a href="https://rsms.me/inter/" target="_blank" className="anchor-text">
           Inter typeface
         </a>
